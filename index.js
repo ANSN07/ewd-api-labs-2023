4590d8844f449d265a589567bfe3523500b78ee7
ANSN07 <115991938+ANSN07@users.noreply.github.com>
2023-05-21 21:48:22 +0000

    Updated Movies Endpoints

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './src/movies';
 import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from "./src/config/dependencies";
+import createMoviesRouter from './src/movies/routes';
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ const dependencies = buildDependencies();
 const port = process.env.PORT;
 app.use(express.json());
 
-app.use('/api', moviesRouter);
+app.use('/api/movies', createMoviesRouter(dependencies));
 app.use('/api/accounts', createAccountsRouter(dependencies));
 
 app.listen(port, () => {
