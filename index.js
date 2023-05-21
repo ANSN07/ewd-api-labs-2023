a3eb3841eda0e9d688a4ef07619e416e1e57f94c
ANSN07 <115991938+ANSN07@users.noreply.github.com>
2023-05-21 20:17:36 +0000

    Added Accounts Endpoints

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies';
+import createAccountsRouter from './src/accounts/routes';
+import buildDependencies from "./src/config/dependencies";
 
 dotenv.config();
 
 const app = express();
+const dependencies = buildDependencies();
 
 const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api', moviesRouter);
+app.use('/api/accounts', createAccountsRouter(dependencies));
 
 app.listen(port, () => {
     console.info(`Server running at ${port}`);
